9683686a4c29b4b9ea4912d47f284867438483c7
Valentina Morelli <valumorelli06@gmail.com>
2026-04-27 13:40:05 -0300

    Ejercicio 2: DOM + fetch

---
+++ ejercicios/c05-async-await-fetch/ej2-lista-html/app.js
@@ -0,0 +1,31 @@
+import { obtenerUsuarios } from '../ej1-usuarios/app.js';
+async function renderizarUsuarios() {
+    const lista = document.querySelector('#listaUsuarios');
+    const carga = document.querySelector('#cargar');
+    const errorD = document.querySelector('#errorVisual');
+    carga.style.display = 'block';
+    errorD.style.display = 'none';
+    lista.innerHTML = '';
+    try {
+        const usuarios = await obtenerUsuarios();
+        if (usuarios.length === 0) {
+            errorD.innerText = "No se encontraron usuarios.";
+            errorD.style.display = 'block';
+        }
+        else {
+            usuarios.forEach(u => {
+                const li = document.createElement('li');
+                li.innerHTML = `<strong>${u.name}</strong> - ${u.email}`;
+                lista.appendChild(li);
+            });
+        }
+    }
+    catch (error) {
+        errorD.innerText = "Hubo un fallo al cargar los usuarios.";
+        errorD.style.display = 'block';
+    }
+    finally {
+        carga.style.display = 'none';
+    }
+}
+renderizarUsuarios();
